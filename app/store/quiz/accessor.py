138fca9216e10b699c67375e83b24e34aa173d6b
Ekaterina Utkina <kate_utkate@vk.com>
2025-03-22 00:59:16 +0300

    add theme accessor

--- app/store/quiz/accessor.py
+++ app/store/quiz/accessor.py
@@ -9,23 +9,27 @@ class QuizAccessor(BaseAccessor):
         return theme
 
     async def get_theme_by_title(self, title: str) -> Theme | None:
-        raise NotImplementedError
+        for theme in self.app.database.themes:
+            if theme.title == title:
+                return theme
 
     async def get_theme_by_id(self, id_: int) -> Theme | None:
-        raise NotImplementedError
+        for theme in self.app.database.themes:
+            if theme.id == id_:
+                return theme
 
     async def list_themes(self) -> list[Theme]:
-        raise NotImplementedError
+        return self.app.database.themes
 
     async def get_question_by_title(self, title: str) -> Question | None:
         raise NotImplementedError
 
     async def create_question(
-        self, title: str, theme_id: int, answers: list[Answer]
+            self, title: str, theme_id: int, answers: list[Answer]
     ) -> Question:
         raise NotImplementedError
 
     async def list_questions(
-        self, theme_id: int | None = None
+            self, theme_id: int | None = None
     ) -> list[Question]:
         raise NotImplementedError
