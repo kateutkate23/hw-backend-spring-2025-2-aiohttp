3f9bfc090625210e72ce7e31c46189748bd5eb91
Ekaterina Utkina <kate_utkate@vk.com>
2025-03-22 04:04:59 +0300

    complete tasks

--- app/store/bot/manager.py
+++ app/store/bot/manager.py
@@ -1,6 +1,6 @@
 import typing
 
-from app.store.vk_api.dataclasses import Update
+from app.store.vk_api.dataclasses import Update, Message
 
 if typing.TYPE_CHECKING:
     from app.web.app import Application
@@ -11,4 +11,8 @@ class BotManager:
         self.app = app
 
     async def handle_updates(self, updates: list[Update]):
-        raise NotImplementedError
+        for update in updates:
+            if update.type == "message_new":
+                user_id = update.object.message.from_id
+                message = Message(user_id=user_id, text="Test message")
+                await self.app.store.vk_api.send_message(message)
--- app/store/vk_api/accessor.py
+++ app/store/vk_api/accessor.py
@@ -1,10 +1,11 @@
 import typing
+import time
 from urllib.parse import urlencode, urljoin
 
 from aiohttp.client import ClientSession
 
 from app.base.base_accessor import BaseAccessor
-from app.store.vk_api.dataclasses import Message
+from app.store.vk_api.dataclasses import Message, UpdateMessage, UpdateObject, Update
 from app.store.vk_api.poller import Poller
 
 if typing.TYPE_CHECKING:
@@ -23,14 +24,17 @@ class VkApiAccessor(BaseAccessor):
         self.ts: int | None = None
 
     async def connect(self, app: "Application"):
-        # TODO: добавить создание aiohttp ClientSession,
-        #  получить данные о long poll сервере с помощью метода groups.getLongPollServer
-        #  вызвать метод start у Poller
-        raise NotImplementedError
+        self.session = ClientSession()
+        await self._get_long_poll_service()
+        self.poller = Poller(app.store)
+        await self.poller.start()
 
     async def disconnect(self, app: "Application"):
-        # TODO: закрыть сессию и завершить поллер
-        raise NotImplementedError
+        if self.poller:
+            await self.poller.stop()
+
+        if self.session:
+            await self.session.close()
 
     @staticmethod
     def _build_query(host: str, method: str, params: dict) -> str:
@@ -38,10 +42,53 @@ class VkApiAccessor(BaseAccessor):
         return f"{urljoin(host, method)}?{urlencode(params)}"
 
     async def _get_long_poll_service(self):
-        raise NotImplementedError
+        url = self._build_query(
+            host="https://api.vk.com/",
+            method="method/groups.getLongPollServer",
+            params={
+                "group_id": self.app.config.bot.group_id,
+                "access_token": self.app.config.bot.token
+            }
+        )
+        response = await self.session.get(url)
+        data = await response.json()
+
+        self.key = data["response"]["key"]
+        self.server = data["response"]["server"]
+        self.ts = data["response"]["ts"]
 
     async def poll(self):
-        raise NotImplementedError
+        params = {"act": "a_check", "key": self.key, "ts": self.ts, "wait": 25}
+        url = f"{self.server}?{urlencode(params)}"
+        response = await self.session.get(url)
+        data = await response.json()
+
+        if data["ts"]:
+            self.ts = data["ts"]
+
+        list_updates = list()
+        for update in data["updates"]:
+            if update["type"] == "message_new":
+                msg = UpdateMessage(
+                    from_id=update["object"]["message"]["from_id"],
+                    id=update["object"]["message"]["id"],
+                    text=update["object"]["message"]["text"]
+                )
+                update_object = UpdateObject(msg)
+                list_updates.append(Update(type=update["type"], object=update_object))
+
+        return list_updates
 
     async def send_message(self, message: Message) -> None:
-        raise NotImplementedError
+        url = self._build_query(
+            host="https://api.vk.com/",
+            method="method/messages.send",
+            params={
+                "user_id": message.user_id,
+                "message": message.text,
+                "access_token": self.app.config.bot.token,
+                "random_id": int(time.time() * 1000)
+            }
+        )
+
+        await self.session.get(url)
